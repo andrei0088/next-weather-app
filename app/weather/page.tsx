2ae983ad02ae69249785970274f8ef69ca167eb9
Andrei D2u <andreidavidoiu@gmail.com>
2025-09-26 15:04:29 +0200

    update4

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -1,18 +1,23 @@
-"use client";
+"use client"; 
 
 import React from "react";
 import { useSearchParams } from "next/navigation";
 import GetMeteo from "../components/GetMeteo";
 
-const Page = () => {
+const WeatherPage = () => {
   const searchParams = useSearchParams();
-  const city = searchParams.get("city") || "Bucharest";
+  const city = searchParams.get("city") ;
 
   return (
     <div className="w-fit mx-auto my-7">
-      <GetMeteo city={city} />
+        <GetMeteo city={city} />
+        {/* Poți adăuga mai multe GetMeteo cu alte orașe aici */}
+      </div>
     </div>
   );
 };
 
-export default Page;
+export default WeatherPage;
+
+
+
