bb3a056829b9c8ca8661d3d329a108f0b7cd0318
Andrei D2u <andreidavidoiu@gmail.com>
2025-09-26 15:23:30 +0200

    update7

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  // Set assetPrefix and basePath for GitHub Pages
-  assetPrefix: process.env.GITHUB_PAGES ? "/next-weather-app/" : "",
-  basePath: process.env.GITHUB_PAGES ? "/next-weather-app" : "",
-};
-
-module.exports = nextConfig;
