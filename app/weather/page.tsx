371b61a5ab477fc42447c008ab2055bf02ddc23a
Andrei D2u <andreidavidoiu@gmail.com>
2025-09-26 14:51:47 +0200

    update 3

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -38,3 +38,21 @@ export default Page;
 
 // Add this to force client-side rendering
 export const dynamic = "force-dynamic";
+("use client");
+import React from "react";
+import { useSearchParams } from "next/navigation";
+import GetMeteo from "../components/GetMeteo";
+
+const Page = () => {
+  const searchParams = useSearchParams();
+  const city = searchParams.get("city") || "Bucharest";
+
+  return (
+    <div className="w-fit mx-auto my-7">
+      <GetMeteo city={city} />
+    </div>
+  );
+};
+
+export default Page;
+export const dynamic = "force-dynamic";
